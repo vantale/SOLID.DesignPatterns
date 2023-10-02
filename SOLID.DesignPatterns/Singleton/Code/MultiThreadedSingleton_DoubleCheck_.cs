a768b1499dfbd23d33b59b466389adc5075b4758
Vantale <vantalo.contact@gmail.com>
2023-10-02 23:02:51 +0200

    Signelton II

---
+++ SOLID.DesignPatterns/Singleton/Code/MultiThreadedSingleton_DoubleCheck_.cs
@@ -0,0 +1,25 @@
+﻿namespace SOLID.DesignPatterns.Singleton.Code
+{
+    public sealed class MultiThreadedSingleton_
+    {
+        private static volatile MultiThreadedSingleton_? _instance;
+        private static readonly object lockObject = new();
+        private MultiThreadedSingleton_() { }
+        public static MultiThreadedSingleton_ GetInstance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (lockObject)
+                    {
+                        if (_instance == null)
+                            _instance = new MultiThreadedSingleton_();
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+
+}
