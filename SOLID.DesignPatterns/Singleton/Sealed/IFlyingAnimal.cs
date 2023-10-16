de1756c5512b916649f5c4d765b2749ba875c305
Vantale <vantalo.contact@gmail.com>
2023-10-16 23:04:11 +0200

    Added Boosting Performance with Sealed Classes

---
+++ SOLID.DesignPatterns/Singleton/Sealed/IFlyingAnimal.cs
@@ -0,0 +1,20 @@
+﻿namespace SOLID.DesignPatterns.Singleton.Sealed
+{
+    public interface IFlyingAnimal { }
+    public class Animal { }
+    public sealed class Husky: Animal { }
+    public class Bear : Animal { }
+
+    internal class CheckInterface
+    {
+        public void Execute()
+        {
+            var husky = new Husky();
+            var bear = new Bear();
+
+
+            var bearAsFlyingAnimal = bear as IFlyingAnimal;
+            var huskyAsFlyingAnimal = husky as IFlyingAnimal;
+        }
+    }
+}
