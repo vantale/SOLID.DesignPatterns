b8f16e8d2a184bf1993141b38c3f269ae419b889
Vantale <vantalo.contact@gmail.com>
2023-10-17 08:59:36 +0200

    Added LazySingleton

---
+++ SOLID.DesignPatterns/Singleton/Lazy/LazySingleton.cs
@@ -0,0 +1,11 @@
+﻿namespace SOLID.DesignPatterns.Singleton.Lazy
+{
+    internal class LazySingleton
+    {
+        private static readonly Lazy<LazySingleton> _lazy = new(() => new LazySingleton());
+        public static LazySingleton Instance { get { return _lazy.Value; } }
+        private LazySingleton()
+        {
+        }
+    }
+}
